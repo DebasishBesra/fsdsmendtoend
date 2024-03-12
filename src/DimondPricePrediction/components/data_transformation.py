a25386f8b931b67b0f43b89fede79663fa109d2e
Debasish Besra <dbesra95@gmail.com>
2024-03-12 06:49:22 +0530

    updated data tranformation file

--- src/DimondPricePrediction/components/data_transformation.py
+++ src/DimondPricePrediction/components/data_transformation.py
@@ -91,8 +91,18 @@ class DataTransformation:
 
             logging.info("Applying preprocessing object on training and testing datasets.")
 
+            train_arr = np.c_[input_feature_train_arr, np.array(target_feature_train_df)]
+            test_arr = np.c_[input_feature_test_arr, np.array(input_feature_test_arr)]
+
             save_object(
                 file_path = self.data_transformation_config.preprocessor_obj_file_path, obj = preprocessing_obj
+            )
+
+            logging.info("preprocessing pickle file saved")
+
+            return (
+                train_arr,
+                test_arr
             )
 
         except Exception as e:
