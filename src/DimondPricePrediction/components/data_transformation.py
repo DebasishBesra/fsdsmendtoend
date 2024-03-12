39800d794dca612b0bcf9e1cb9b7249c9c4a7141
Debasish Besra <dbesra95@gmail.com>
2024-03-12 08:23:24 +0530

    Code updated

--- src/DimondPricePrediction/components/data_transformation.py
+++ src/DimondPricePrediction/components/data_transformation.py
@@ -7,7 +7,7 @@ from dataclasses import dataclass
 from src.DimondPricePrediction.exception import customexception
 from src.DimondPricePrediction.logger import logging
 
-from sklearn.compose import     ColumnTransformer   
+from sklearn.compose import ColumnTransformer   
 from sklearn.impute import SimpleImputer
 from sklearn.pipeline import Pipeline
 from sklearn.preprocessing import OrdinalEncoder,StandardScaler
@@ -84,6 +84,9 @@ class DataTransformation:
 
             input_feature_train_df = train_df.drop(columns = drop_columns,axis=1)
             target_feature_train_df = train_df[target_column_name]
+
+            input_feature_test_df = test_df.drop(columns = drop_columns,axis=1)
+            target_feature_test_df = test_df[target_column_name]
 
             input_feature_train_arr = preprocessing_obj.fit_transform(input_feature_train_df)
 
